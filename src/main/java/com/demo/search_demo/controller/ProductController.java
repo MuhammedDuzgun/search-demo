c566be83c1285d83afbbc7c170b4b1e9a3448108
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-09-12 00:01:25 +0300

    add product controller

---
+++ src/main/java/com/demo/search_demo/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.demo.search_demo.controller;
+
+import com.demo.search_demo.entity.Product;
+import com.demo.search_demo.service.IProductService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final IProductService productService;
+
+    public ProductController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam("query") String query) {
+        return ResponseEntity.ok(productService.searchProducts(query));
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+}
